6eed1b932e8bfebb0a911c01367bf52b92c5afbd
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-01-13 16:58:35 -0800

    [Home.js] Setting up components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Home />
     </BrowserRouter>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="page-container">
+      <div className="title">
+        {/* <img src="https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80" /> */}
+          <h1>New Star Maintenance</h1>
+          <p>Restaurant Janitorial</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
